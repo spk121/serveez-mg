fb32d5d2ddff7250680a056f9b61c2e9edfc4a1e
Michael Gran <spk121@yahoo.com>
2010-08-26 06:33:23 -0700

    Reduce the root directory scripts to the minimum required
    The following files are removed
    
    * INSTALL
    * README-Win32
    * README-alpha
    * README-cross
    * RELEASE
    * TODO
    * acinclude.m4
    * autodsp
    * autogen.sh
    * config.h.serveez
    * debian.sh.in
    * redhat.sh.in
    * serveez.ap
    * vcgen.bat
    * version.h.serveez

--- vcgen.bat
+++
@@ -1,5 +0,0 @@
-@set GUILEPATH=..\guile
-copy config.h.serveez config.h
-copy version.h.serveez version.h
-copy src\svzconfig.h.serveez src\svzconfig.h
-copy src\libserveez\svzpath.h.serveez src\libserveez\svzpath.h
